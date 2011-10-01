fd43a165edab1ecd6686ed7e5dd2bbd9577e5a64
Travis Fischer <travis@smacktweets.com>
2011-10-01 11:55:05 -0700

    Added rts

--- tweepy/api.py
+++ tweepy/api.py
@@ -42,7 +42,7 @@ class API(object):
     home_timeline = bind_api(
         path = '/statuses/home_timeline.json',
         payload_type = 'status', payload_list = True,
-        allowed_param = ['since_id', 'max_id', 'count', 'page'],
+        allowed_param = ['since_id', 'max_id', 'count', 'page', 'include_rts'],
         require_auth = True
     )
 
